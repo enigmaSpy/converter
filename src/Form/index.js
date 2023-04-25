ee003ede9cb046c6faea2866f2a2e6913017b7c8
Kamil Soczewica <soczekv18@gmail.com>
2023-04-25 16:35:00 +0200

    added interaction

---
+++ src/Form/index.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+
+const Form = () => {
+    const [value, setValue] = useState("");
+    const [convertedValue, setConvertedValue] = useState("0.00");
+    const [currencyOwn, setOvnedCurrency] = useState("PLN")
+    const [currencyWanted, setWantedCurrency] = useState("EUR")
+    const currencyRate = {
+        PLN: 1,
+        USD: 0.23,
+        EUR: 0.21,
+        JPY: 31.26
+    };
+    const convertValue = () => {
+        let result = 0;
+        result = (value * currencyRate[currencyWanted] *currencyRate[currencyOwn])
+        setConvertedValue(result.toFixed(2));
+    }
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+        convertValue();
+    }
+    return (
+        <form action="#" className="form" onSubmit={onFormSubmit}>
+
+            <fieldset className="form__feildset">
+
+                <label className="form__item">
+                    <span className="form__itemText">Mam:</span>
+                    <input type="number" name="own" value={value} onChange={({ target }) => setValue(target.value)} className="form__input" min="0" required />
+                </label>
+
+                <select
+                    aria-label="Wybierz swoją walute"
+                    value={currencyOwn}
+                    onChange={({ target }) => setOvnedCurrency(target.value)}
+                    name="currencyOwn"
+                    className="form__select">
+                    <option >PLN</option>
+                    <option >EUR</option>
+                    <option >USD</option>
+                    <option >JPY</option>
+                </select>
+
+            </fieldset>
+            <button className="form__reverseButton">💱</button>
+
+            <fieldset className="form__feildset">
+                <label className="form__item">
+                    <span className="form__itemText"> Otrzymam: </span>
+                    <input type="number" name="offert" value={convertedValue} className="form__input form__input--valueAfter" disabled />
+                </label>
+                <select
+                    aria-label="Wybierz walute na którą chcesz"
+                    value={currencyWanted}
+                    onChange={({ target }) => setWantedCurrency(target.value)}
+                    name="currencyOffert"
+                    className="form__select">
+                    <option >PLN</option>
+                    <option >EUR</option>
+                    <option >USD</option>
+                    <option >JPY</option>
+                </select>
+            </fieldset>
+
+            <button type="submit" className="form__calculate">Przelicz</button>
+
+        </form>
+    );
+}
+export default Form;
---
+++ src/Table/index.js
@@ -0,0 +1,27 @@
+
+const Table=()=>(
+    <table className="priceTable">
+        <thead className="priceTable__thead">
+            <tr className="priceTable__tr">
+                <th className="priceTable__th">1</th>
+                <th className="priceTable__th priceTable__td--currency">PLN</th>
+            </tr>
+          </thead>
+          <tbody className="priceTable__tbody">
+            <tr  className="priceTable__tr ">
+                <td className="priceTable__td">0,23</td>
+                <td className="priceTable__td priceTable__td--currency">USD</td>
+            </tr>
+            <tr className="priceTable__tr">
+                <td className="priceTable__td">0,21</td>
+                <td className="priceTable__td priceTable__td--currency">EUR</td>
+            </tr>
+            <tr className="priceTable__tr">
+                <td className="priceTable__td">31,26</td>
+                <td className="priceTable__td priceTable__td--currency">JPY</td>
+            </tr>
+          </tbody>
+      </table>
+);
+
+export default Table;
