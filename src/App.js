ace1ed4f328a4ba7f579597ec066967d8532ad39
Kamil Soczewica <soczekv18@gmail.com>
2023-04-25 14:08:06 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <Header/>
+    <main className="container">
+
+      <form action="#" className="form">
+        <div className="form__wrapper">
+          
+          <fieldset className="form__feildset">
+            <label className="form__item">
+              <span className="form__itemText">Mam:</span>
+              <input type="number" name="own" className="form__input form__input--valueBefore" min="0"  required />
+            </label>
+            <select aria-label="Wybierz swoją walute" name="currencyOwn" className="form__select form__select--currency">
+              <option value="pln" selected>PLN</option>
+              <option value="eur">EUR</option>
+              <option value="usd">USD</option>
+              <option value="jpy">JPY</option>
+            </select>
+          </fieldset>
+
+              <button className="form__reverseButton">💱</button>
+
+          <fieldset className="form__feildset">
+            <label className="form__item">
+              <span className="form__itemText"> Otrzymam: </span>
+              <input type="number" name="offert" className="form__input form__input--valueAfter" value="0.00" disabled/>
+            </label>
+            <select aria-label="Wybierz walute na którą chcesz" name="currencyOffert" className="form__select form__select--currencyWanted" >
+              <option value="pln">PLN</option>
+              <option value="eur" selected>EUR</option>
+              <option value="usd">USD</option>
+              <option value="jpy">JPY</option>
+            </select>
+          </fieldset>
+
+          <button type="submit" className="form__calculate">Przelicz</button>
+        </div>
+      </form>
+
+      <table className="priceTable">
+        <thead className="priceTable__thead">
+            <tr className="priceTable__tr">
+                <th className="priceTable__th">1</th>
+                <th className="priceTable__th priceTable__td--currency">PLN</th>
+            </tr>
+          </thead>
+          <tbody className="priceTable__tbody">
+            <tr  className="priceTable__tr ">
+                <td className="priceTable__td">0,23</td>
+                <td className="priceTable__td priceTable__td--currency">USD</td>
+            </tr>
+            <tr className="priceTable__tr">
+                <td className="priceTable__td">0,21</td>
+                <td className="priceTable__td priceTable__td--currency">EUR</td>
+            </tr>
+            <tr className="priceTable__tr">
+                <td className="priceTable__td">31,26</td>
+                <td className="priceTable__td priceTable__td--currency">JPY</td>
+            </tr>
+          </tbody>
+      </table>
+    </main>
+   </>
   );
 }
 
---
+++ src/Header/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+const Header = ()=>(
+    <header>
+        <h1 className="header__title">Kantor</h1>
+    </header>
+);
+export default Header;
