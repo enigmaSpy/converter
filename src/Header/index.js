fbede93bac6efd9e18dcadfcf55972d32dabee9d
Kamil Soczewica <soczekv18@gmail.com>
2023-04-26 23:06:27 +0200

    added timer

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,8 +1,22 @@
+import React,{useState, useEffect} from 'react';
 import './style.css';
 
-const Header = ()=>(
-    <header>
-        <h1 className="header__title">Kantor</h1>
-    </header>
-);
+const Header = () => {
+    const [date, setDate]= useState(new Date().toLocaleString());
+    
+    useEffect(()=>{
+        const intervalId = setInterval(()=>{
+            setDate(new Date().toLocaleString())
+        });
+
+        return ()=> clearInterval(intervalId);
+    },[date]);
+
+    return (
+        <header>
+            <h1 className="header__title">Kantor</h1>
+            <p className='header__data'>{date}</p>
+        </header>
+    )
+};
 export default Header;
